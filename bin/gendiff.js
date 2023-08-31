54988a790aee1468635df69f7c9d893bdd91f75e
atrya-trezer <fedya.mazin@gmail.com>
2023-08-31 15:23:58 +0300

    parse

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,11 +7,26 @@ const program = new Command();
 const genDiff = (filepath1, filepath2, type) => {
   let data1, data2;
   try { data1 = readFileSync(filepath1, "utf8")
-  console.log("File content:", data1);
+    //console.log("File content:", data1);
 } catch (err) { console.error(err); }
   try { data2 = readFileSync(filepath2, "utf8")
-  console.log("File content:", data2);
+    //console.log("File content:", data2);
 } catch (err) { console.error(err); }
+  if (data1 && data2){
+    let json_data1, json_data2
+    try {
+      json_data1 = JSON.parse(data1)
+      console.log(json_data1)
+    } catch(err) {
+      console.error(err)
+    }
+    try {
+      json_data2 = JSON.parse(data2)
+      console.log(json_data2)
+    } catch(err) {
+      console.error(err)
+    }
+  }
 };
 
 program
