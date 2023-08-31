73c832773c65fb7b8da4e9c628767ead01e1ff4d
atrya-trezer <fedya.mazin@gmail.com>
2023-08-31 19:02:32 +0300

    test running

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,6 +2,15 @@ import test from 'node:test';
 import { expect } from 'expect';
 import genDiff from '../src/diff.js';
 
+const res = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`
+
 test('genDiff', () => {
-  expect(genDiff('../data/file1.json', '../data/file2.json')).toBe(3);
+  expect(genDiff('./data/file1.json', './data/file2.json', {})).toBe(res);
 });
