6314e453c1da5ad3a8b46002c84341f654301c28
atrya-trezer <fedya.mazin@gmail.com>
2023-08-31 13:45:47 +0300

    rabotaet

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,8 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .option('-V, --version', 'output the version number')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .parse(process.argv);
