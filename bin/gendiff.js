e9bcb60b2ebdeae307e14875728734d044339b27
atrya-trezer <fedya.mazin@gmail.com>
2023-08-31 18:31:05 +0300

    lint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,60 +1,65 @@
 import { Command } from 'commander';
 import { readFileSync } from 'node:fs';
 import _ from 'lodash';
-const program = new Command();
 
-const main = (filepath1, filepath2, type) => {
-  console.log(genDiff(filepath1, filepath2, type));
-}
+const program = new Command();
 
 const genDiff = (filepath1, filepath2, type) => {
   let output = '';
-  let data1 = readFileSync(filepath1, "utf8");
-  let data2 = readFileSync(filepath2, "utf8");
-  if (data1 && data2){
-    let diff = {};
-    let json_data1 = JSON.parse(data1);
-    let json_data2 = JSON.parse(data2);
-    if (json_data1 && json_data2) {
-      for (const key in json_data1) {
-        if (!_.has(json_data2, key)){
-          diff[key] = {'deleted': true}
+  const data1 = readFileSync(filepath1, 'utf8');
+  const data2 = readFileSync(filepath2, 'utf8');
+  if (data1 && data2) {
+    const diff = {};
+    const jsonData1 = JSON.parse(data1);
+    const jsonData2 = JSON.parse(data2);
+    if (jsonData1 && jsonData2) {
+      const jsonData1Keys = Object.keys(jsonData1);
+      for (let i = 0; i < jsonData1Keys.length; i += 1) {
+        const key = jsonData1Keys[i];
+        if (!_.has(jsonData2, key)) {
+          diff[key] = { deleted: true };
+        } else if (jsonData1[key] !== jsonData2[key]) {
+          diff[key] = { updated: true };
         } else {
-          if (json_data1[key] !== json_data2[key]){
-            diff[key] = {'updated': true}
-          } else {
-            diff[key] = {'unchanged': true}
-          }
+          diff[key] = { unchanged: true };
         }
       }
-      for (const key in json_data2) {
-        if (!_.has(json_data1, key)){
-          diff[key] = {'added': true}
+      const jsonData2Keys = Object.keys(jsonData2);
+      for (let i = 0; i < jsonData2Keys.length; i += 1) {
+        const key = jsonData2Keys[i];
+        if (!_.has(jsonData1, key)) {
+          diff[key] = { added: true };
         }
       }
     } else {
-      console.error('Bad files provided')
+      console.error('Bad files provided');
     }
-    //console.log(diff)
-    const sortedKeys = _.sortBy(Object.keys(diff))
-    output += '{\n';
-    for (const key of sortedKeys){
-      if (diff[key]['unchanged']){
-        output += `    ${key}: ${json_data1[key]}\n`;
-      } else if (diff[key]['deleted']){
-        output += `  - ${key}: ${json_data1[key]}\n`;
-      } else if (diff[key]['added']) {
-        output += `  + ${key}: ${json_data2[key]}\n`;
-      }else if (diff[key]['updated']) {
-        output += `  - ${key}: ${json_data1[key]}\n`;
-        output += `  + ${key}: ${json_data2[key]}\n`;
+    const sortedKeys = _.sortBy(Object.keys(diff));
+    if (!type.format) {
+      output += '{\n';
+      for (let i = 0; i < sortedKeys.length; i += 1) {
+        const key = sortedKeys[i];
+        if (diff[key].unchanged) {
+          output += `    ${key}: ${jsonData1[key]}\n`;
+        } else if (diff[key].deleted) {
+          output += `  - ${key}: ${jsonData1[key]}\n`;
+        } else if (diff[key].added) {
+          output += `  + ${key}: ${jsonData2[key]}\n`;
+        } else if (diff[key].updated) {
+          output += `  - ${key}: ${jsonData1[key]}\n`;
+          output += `  + ${key}: ${jsonData2[key]}\n`;
+        }
       }
+      output += '}';
     }
-    output += '}'; 
   } else {
-    console.error('Bad filenames provided')
+    console.error('Bad filenames provided');
   }
   return output;
+};
+
+const main = (filepath1, filepath2, type) => {
+  console.log(genDiff(filepath1, filepath2, type));
 };
 
 program
