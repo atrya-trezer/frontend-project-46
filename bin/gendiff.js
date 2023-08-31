10c21d3b9f8598781984f4f0cadc5b1596bddf5c
atrya-trezer <fedya.mazin@gmail.com>
2023-08-31 14:55:37 +0300

    try

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,16 @@ import { readFileSync } from 'node:fs';
 const program = new Command();
 
 const genDiff = (filepath1, filepath2, type) => {
+  let data1, data2;
+  try { data1 = readFileSync(filepath1, "utf8")
+  console.log("File content:", data1);
+} catch (err) { console.error(err); }
+  try { data2 = readFileSync(filepath2, "utf8")
+  console.log("File content:", data2);
+} catch (err) { console.error(err); }
+
+
+
   console.log(readFileSync(filepath1, "utf8"));
   console.log(readFileSync(filepath2,"utf8"));
   console.log(type);
