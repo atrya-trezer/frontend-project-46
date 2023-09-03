f3fd906bac7e3ec5abbf76b60dbec8dfaacee0e4
atrya-trezer <fedya.mazin@gmail.com>
2023-09-03 14:08:21 +0300

    yml

---
+++ __tests__/gendiffYml.test.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/diff.js';
+
+const res = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('genDiff', () => {
+  expect(genDiff('./data/file1.yml', './data/file2.yml', {})).toBe(res);
+});
