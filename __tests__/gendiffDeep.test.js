ef2b14f3222b21b3ca07c40897688401ffee2be5
atrya-trezer <fedya.mazin@gmail.com>
2023-10-24 18:24:14 +0300

    added extantion fixtures

--- __tests__/gendiffDeep.test.js
+++ __tests__/gendiffDeep.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 import readFile from '../src/index_test.js';
 
-const res = readFile('expected_gendiffDeep');
+const res = readFile('expected_gendiffDeep.txt');
 
 test('genDiff', () => {
   expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'stylish')).toBe(res);
--- __tests__/gendiffYml.test.js
+++ __tests__/gendiffYml.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 import readFile from '../src/index_test.js';
 
-const res = readFile('expected_gendiffYml');
+const res = readFile('expected_gendiffYml.txt');
 
 test('genDiff', () => {
   expect(genDiff('./__fixtures__/file1.yml', './__fixtures__/file2.yml', 'stylish')).toBe(res);
